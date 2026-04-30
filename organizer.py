aa7a9448f8c0d7171ca9675389a0853e8d95626a
lower987 <mlscoot3608@gmail.com>
2026-04-30 04:03:56 -0400

    Delete organizer.py

--- organizer.py
+++
@@ -1,8 +0,0 @@
-print("-------------------------------------------")
-print("This will be your organizer for free time!!")
-print("-------------------------------------------")
-print()
-w = input("Write something: ")
-def main():
-    print(w)
-main()
